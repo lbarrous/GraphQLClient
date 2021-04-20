1384d3db035f8f5c61107499a6932c023f71ce57
Alvaro Perez Garcia <apgarcia@MacBook-Pro-de-Alvaro.local>
2021-04-20 12:19:20 +0200

    Initiating client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ApolloClient, InMemoryCache} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API,
+  cache: new InMemoryCache()
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div><h2>My Muscle App</h2></div>
+  </ApolloProvider>
+);
 
 export default App;
